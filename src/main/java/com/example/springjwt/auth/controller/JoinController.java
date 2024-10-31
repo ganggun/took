73ac6c3c54a4ce3bbee03f54b18f85dfafc36baa
ganggun <165283835+ganggun@users.noreply.github.com>
2024-10-31 19:45:28 +0900

    추가

---
+++ src/main/java/com/example/springjwt/auth/controller/JoinController.java
@@ -0,0 +1,27 @@
+package com.example.springjwt.auth.controller;
+
+import com.example.springjwt.auth.dto.JoinDTO;
+import com.example.springjwt.auth.service.JoinService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@ResponseBody
+public class JoinController {
+
+    private final JoinService joinService;
+
+    public JoinController(JoinService joinService) {
+
+        this.joinService = joinService;
+    }
+
+    @PostMapping("/join")
+    public String joinProcess(JoinDTO joinDTO) {
+
+        joinService.joinProcess(joinDTO);
+
+        return "ok";
+    }
+}
