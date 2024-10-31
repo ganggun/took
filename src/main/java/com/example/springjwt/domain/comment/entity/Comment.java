19eb7514ece1904c77a62741c14b1d976323df34
sehyuk080101 <164876765+sehyuk080101@users.noreply.github.com>
2024-10-31 20:58:34 +0900

    change

--- src/main/java/com/example/springjwt/domain/comment/entity/Comment.java
+++ src/main/java/com/example/springjwt/domain/comment/entity/Comment.java
@@ -1,15 +1,22 @@
 package com.example.springjwt.domain.comment.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.Data;
 
 @Data
+@Entity
 public class Comment {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private Long userId;
+    private String writer;
 
     private String content;
 
-    private Long like;
+    private Long like = 0L;
 }
